8be6fa89a1fab793d94a2c032a3ae4d25db43488
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-31 00:23:43 +0900

    update readme

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,18 +1,21 @@
-import { API_URL } from "../../../(home)/page";
-
-async function getMovie(id: string) {
-    return fetch(`${API_URL}/${id}`).then((response) => response.json());
-}
-
-async function getVideos(id: string) {
-    return fetch(`${API_URL}/${id}/videos`).then((response) => response.json());
-}
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
 export default async function MovieDetail({
     params: { id },
 }: {
     params: { id: string };
 }) {
-    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-    return <h1>{movie.title}</h1>;
+    return (
+        <div>
+            <Suspense fallback={<h1>Loading movie info</h1>}>
+                <MovieInfo id={id} />
+            </Suspense>
+
+            <Suspense fallback={<h1>Loading movie video</h1>}>
+                <MovieVideos id={id} />
+            </Suspense>
+        </div>
+    );
 }
