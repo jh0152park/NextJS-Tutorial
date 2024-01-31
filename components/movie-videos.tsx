3398a9f86321db9ad3d2876eeb66b2c358252a0c
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-31 17:15:43 +0900

    added movie css

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import style from "../styles/movie-videos.module.css";
 
 export async function getVideos(id: string) {
     return fetch(`${API_URL}/${id}/videos`).then((response) => response.json());
@@ -6,5 +7,17 @@ export async function getVideos(id: string) {
 
 export default async function MovieVideos({ id }: { id: string }) {
     const videos = await getVideos(id);
-    return <h5>{JSON.stringify(videos)}</h5>;
+    return (
+        <div className={style.container}>
+            {videos.map((video, index) => (
+                <iframe
+                    key={index}
+                    src={`https://youtube.com/embed/${video.key}`}
+                    title={video.name}
+                    allow="accelerometer; autoplay clipboard-write; encryped-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                />
+            ))}
+        </div>
+    );
 }
